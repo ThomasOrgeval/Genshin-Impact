11280492cfecc70ab84df2104f02d7c535e14747
Thomas Orgeval <orgevalthomas@gmail.com>
2021-04-24 18:23:30 +0200

    elements$

--- APP/resources/script.js
+++ APP/resources/script.js
@@ -1,5 +1,5 @@
 function slug(text) {
-    return text;
+    return text.replace(' ', '-');
 }
 
 function getIcon(type, id) {
@@ -7,10 +7,12 @@ function getIcon(type, id) {
         method: 'get',
         url: 'https://genshin-api.thomasorgeval.fr/api/' + type + '/' + id
     }).then(function (response) {
-
+        console.log(response.data);
+        return 'https://genshin.thomasorgeval.fr/resources/images/' + type + '/' + slug(response.data.label) + '.png';
     }).catch(function (error) {
         console.log(error);
     });
+    return null;
 }
 
 $(document).ready(function ($) {
@@ -25,9 +27,17 @@ $(document).ready(function ($) {
                 $.get(
                     'https://genshin.thomasorgeval.fr/resources/templates/character.html'
                 ).done(function (html) {
-                    $('#char' + data.id).find('img.img-fluid').attr('src', 'https://genshin.thomasorgeval.fr/resources/characters/' + slug(data.label) + '.png')
-                    $('#char' + data.id).append(html).find('h5.character_name').append(data.label);
-                    $('#char' + data.id).find('p.card-text').html(data.lvl_up_material1 + ' ' + data.lvl_up_material2);
+                    let id = '#char' + data.id;
+                    $(id).append(html).find('h5.character_name').append(data.label);
+
+                    // Reprise de l'image du character
+                    $(id).find('img.img-fluid').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/characters/' + slug(data.label) + '.png');
+                    // Reprise de l'icone de l'élément du character
+                    $(id).find('.row-cols-2 img').attr('src', getIcon('elements', data.element))
+                    // Reprise des matériaux d'amélioration du character
+                    $(id).find('p.card-text').html(data.lvl_up_material1 + ' ' + data.lvl_up_material2 + ' ' + data.lvl_up_material3);
+                    // Reprise de talents du character
+                    $(id).find('p.card-text').append(' ' + data.talent_up_material1 + ' ' + data.talent_up_material2 + ' ' + data.talent_up_material3);
                 });
             });
 
