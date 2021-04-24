b5291a92550f2dc531c372d1959efd3505050f5e
Thomas Orgeval <orgevalthomas@gmail.com>
2021-04-24 20:51:21 +0200

    update

--- APP/resources/script.js
+++ APP/resources/script.js
@@ -22,25 +22,24 @@ $(document).ready(function ($) {
                     $(id).append(html).find('.character_name').append(data.label);
 
                     // Reprise de l'image du character
-                    $(id).find('.img-fluid').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/characters/' + slug(data.label) + '.png');
+                    $(id).find('.img-fluid').attr('src', './resources/images/characters/' + slug(data.label) + '.png');
 
                     Promise.all([getIcon('element', data.element), getIcon('item', data.lvl_up_material1)
-                        , getIcon('item', data.lvl_up_material2), getIcon('item', data.lvl_up_material3), getIcon('item', data.talent_up_material3)
-                        , getIcon('item', data.talent_up_material3), getIcon('item', data.talent_up_material3)]).then(function (results) {
+                        , getIcon('item', data.lvl_up_material2), getIcon('item', data.lvl_up_material3), getIcon('item', data.talent_up_material1)
+                        , getIcon('item', data.talent_up_material2), getIcon('item', data.talent_up_material3)]).then(function (results) {
 
                         // Reprise de l'icone de l'élément du character
                         $(id).find('.element').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/elements/' + slug(results[0].label) + '.png');
-
                         // Reprise des matériaux d'amélioration du character
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[1].label) + results[1].rarity_max + '.png" alt="lvl_up_material1">');
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[2].label) + results[2].rarity_max + '.png" alt="lvl_up_material2">');
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[3].label) + results[3].rarity_max + '.png" alt="lvl_up_material3">');
-                        $(id).find('.card-text').append('<span class="text-black">&#124;</span>');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[1].label) + results[1].rarity_max + '.png" alt="lvl_up_material1">');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[2].label) + results[2].rarity_max + '.png" alt="lvl_up_material2">');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[3].label) + results[3].rarity_max + '.png" alt="lvl_up_material3">');
+                        $(id).find('.card-text').append('<div class="vertical"></div>');
 
                         // Reprise de talents du character
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[4].label) + results[4].rarity_max + '.png" alt="talent_up_material1">');
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[5].label) + results[5].rarity_max + '.png" alt="talent_up_material2">');
-                        $(id).find('.card-text').append('<img class="item" src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[6].label) + results[6].rarity_max + '.png" alt="talent_up_material3">');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[4].label) + results[4].rarity_max + '.png" alt="talent_up_material1">');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[5].label) + results[5].rarity_max + '.png" alt="talent_up_material2">');
+                        $(id).find('.card-text').append('<img class="item" src="./resources/images/items/' + slug(results[6].label) + results[6].rarity_max + '.png" alt="talent_up_material3">');
                     });
                 });
             });
