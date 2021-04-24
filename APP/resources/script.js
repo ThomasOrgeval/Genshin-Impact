172f2fcb8b1b099fd20cdc42d12f887b981ea8ba
Thomas Orgeval <orgevalthomas@gmail.com>
2021-04-24 19:51:59 +0200

    up items

--- APP/resources/script.js
+++ APP/resources/script.js
@@ -19,20 +19,28 @@ $(document).ready(function ($) {
                     'https://genshin.thomasorgeval.fr/resources/templates/character.html'
                 ).done(function (html) {
                     let id = '#char' + data.id;
-                    $(id).append(html).find('h5.character_name').append(data.label);
+                    $(id).append(html).find('.character_name').append(data.label);
 
                     // Reprise de l'image du character
-                    $(id).find('img.img-fluid').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/characters/' + slug(data.label) + '.png');
-                    // Reprise de l'icone de l'élément du character
-                    Promise.all([getIcon('element', data.element)]).then(function (results) {
-                        const acct = results[0];
-                        console.log(acct);
-                        $(id).find('img.element').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/elements/' + slug(acct.label) + '.png');
+                    $(id).find('.img-fluid').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/characters/' + slug(data.label) + '.png');
+
+                    Promise.all([getIcon('element', data.element), getIcon('item', data.lvl_up_material1)
+                        , getIcon('item', data.lvl_up_material2), getIcon('item', data.lvl_up_material3), getIcon('item', data.talent_up_material3)
+                        , getIcon('item', data.talent_up_material3), getIcon('item', data.talent_up_material3)]).then(function (results) {
+
+                        // Reprise de l'icone de l'élément du character
+                        $(id).find('.element').attr('src', 'https://genshin.thomasorgeval.fr/resources/images/elements/' + slug(results[0].label) + '.png');
+
+                        // Reprise des matériaux d'amélioration du character
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[1].label) + '.png" alt="lvl_up_material1">');
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[2].label) + '.png" alt="lvl_up_material2">');
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[3].label) + '.png" alt="lvl_up_material3">');
+
+                        // Reprise de talents du character
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[4].label) + '.png" alt="talent_up_material1">');
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[5].label) + '.png" alt="talent_up_material2">');
+                        $(id).find('.card-text').append('<img src="https://genshin.thomasorgeval.fr/resources/images/items/' + slug(results[6].label) + '.png" alt="talent_up_material3">');
                     });
-                    // Reprise des matériaux d'amélioration du character
-                    $(id).find('p.card-text').html(data.lvl_up_material1 + ' ' + data.lvl_up_material2 + ' ' + data.lvl_up_material3);
-                    // Reprise de talents du character
-                    $(id).find('p.card-text').append(' ' + data.talent_up_material1 + ' ' + data.talent_up_material2 + ' ' + data.talent_up_material3);
                 });
             });
 
